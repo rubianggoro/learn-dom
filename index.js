6772d30b1ae851c30a90e5531e8ae33cb9e3b793
rubianggoro <rubi.anggoro199@gmail.com>
2020-03-31 08:39:04 +0700

    Update

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ let add = document.getElementById("btn");
 add.addEventListener("click", addInput);
 
 function addInput() {
-  document.getElementById("list").innerHTML += document.getElementById(
-    "input"
-  ).value;
+  var data = document.getElementById("input").value;
+  var ul = document.createElement("ul");
+  var list = document.createElement("li");
+  ul.appendChild(list)
+  list.innerHTML = data;
+  document.body.appendChild(ul);
 }
